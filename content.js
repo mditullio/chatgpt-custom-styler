28285ce3fc5d97bb48a99e77dfa0b0352ce9faa0
mditullio <47209152+mditullio@users.noreply.github.com>
2024-09-15 10:57:05 +0200

    Reapply column width everytime it's changed.

--- content.js
+++ content.js
@@ -2,20 +2,36 @@
 
 (function () {
 
-    document.addEventListener("readystatechange", () => {
+    document.addEventListener('readystatechange', () => {
         
         setTimeout(() => {
+
             /** @type HTMLDivElement */
-            const leftColumn = document.querySelector("div.flex-shrink-0.overflow-x-hidden.bg-token-sidebar-surface-primary");
+            const leftColumn = document.querySelector('div.flex-shrink-0.overflow-x-hidden.bg-token-sidebar-surface-primary');
+
+            /** @type MutationObserverInit */
+            const observerConfig = {
+                attributes: true,
+                attributeFilter: ['style'],
+            };
+        
+            const setLeftColumnWidth = () => {
+                if (leftColumn.style.width == '260px') {
+                    leftColumn.style.width = '400px';
+
+                    /** @type HTMLDivElement */
+                    const chatHistory = leftColumn.firstChild;
+                    chatHistory.classList.remove('w-[260px]');
+                }
+            }
 
             if (leftColumn) {
-                leftColumn.style.width = "400px";
+                setLeftColumnWidth();    
+                const observer = new MutationObserver(setLeftColumnWidth);
+                observer.observe(leftColumn, observerConfig);                
+            }           
 
-                /** @type HTMLDivElement */
-                const chatHistory = leftColumn.firstChild;
-                chatHistory.classList.remove("w-[260px]");
-            }
-        }, 1000);
+        }, 500);
     });
 
 })();
