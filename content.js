5a03a262ea351ad8ef0339bd98be9d14c23b6424
mditullio <47209152+mditullio@users.noreply.github.com>
2024-09-15 15:07:10 +0200

    Handle body changes.

--- content.js
+++ content.js
@@ -2,33 +2,55 @@
 
 (function () {
 
-    document.addEventListener('readystatechange', () => {
+    let sidebarSelector = 'div.flex-shrink-0.overflow-x-hidden.bg-token-sidebar-surface-primary';
 
-        /** @type HTMLDivElement */
-        const sidebar = document.querySelector('div.flex-shrink-0.overflow-x-hidden.bg-token-sidebar-surface-primary');
+    /** @type HTMLDivElement */
+    let sidebar = null;
 
-        if (sidebar) {
-            /** @type MutationObserverInit */
-            const observerConfig = {
-                attributes: true,
-                attributeFilter: ['style'],
-            };
+    /** @type MutationObserver */
+    let bodyObserver = null;
 
-            const setSidebarWidth = () => {
-                if (sidebar.style.width == '260px') {
-                    sidebar.style.width = '400px';
+    /** @type MutationObserver */
+    let sidebarObserver = null;
 
-                    /** @type HTMLDivElement */
-                    const chatHistory = sidebar.firstChild;
-                    chatHistory.classList.remove('w-[260px]');
-                }
-            }
-
-            setSidebarWidth();
-            const observer = new MutationObserver(setSidebarWidth);
-            observer.observe(sidebar, observerConfig);
+    /** Adapts sidebar width */
+    const applyCustomStyles = () => {
+        if (sidebar?.style.width == '260px') {
+            sidebar.style.width = '400px';
+            /** @type HTMLDivElement */
+            const chatHistory = sidebar.firstChild;
+            chatHistory.classList.remove('w-[260px]');
         }
+    }
 
+    /** Observe for body changes */
+    const observeBody = () => {
+        bodyObserver?.disconnect();
+        applyCustomStyles();
+        bodyObserver = new MutationObserver(observeSidebar);
+        bodyObserver.observe(document.body, {
+            childList: true
+        });
+    }
+
+    /** Observe for sidebar changes */
+    const observeSidebar = () => {
+        sidebarObserver?.disconnect();
+        sidebar = document.querySelector(sidebarSelector);
+        applyCustomStyles();
+        sidebarObserver = new MutationObserver(applyCustomStyles);
+        sidebarObserver.observe(sidebar, {
+            attributes: true,
+            attributeFilter: ['style'],
+        });
+    }
+
+    /** Initialize */
+    document.addEventListener('readystatechange', () => {
+        sidebar = document.querySelector(sidebarSelector);
+        applyCustomStyles();
+        observeBody();
+        observeSidebar();
     });
 
 })();
